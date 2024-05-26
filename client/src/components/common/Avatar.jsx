09311ad71bb3820e6d675b8e9c350855f8104809
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-05-26 23:45:50 +0600

    profile camera icon add context menu todo

--- client/src/components/common/Avatar.jsx
+++ client/src/components/common/Avatar.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { FaCamera } from "react-icons/fa6";
 
-function Avatar() {
-  return <div>Avatar</div>;
+function Avatar({ type, image, setImage }) {
+  const [hover, setHover] = useState(false);
+  const [isContextMenuVisible , setIsContextMenuVisible ] = useState(false)
+  const [contextMenuCordinates, setcontextMenuCordinates] = useState({
+    x:0,
+    y:0,
+  })
+  const showContextMenu = (e) => {
+    e.preventDefault();
+    setcontextMenuCordinates({x: e.pageX, Y: e.pageY});
+    setIsContextMenuVisible(true);
+  }
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        {type === "sm" && (
+          <div className="relative h-10 w-10">
+            <Image
+              src={image}
+              alt="avatar"
+              className="rounded-full"
+              width={40}
+              height={40}
+            />
+          </div>
+        )}
+        {type === "lg" && (
+          <div className="relative h-14 w-14">
+            <Image
+              src={image}
+              alt="avatar"
+              className="rounded-full"
+              width={56}
+              height={56}
+            />
+          </div>
+        )}
+        {type === "xl" && (
+          <div
+            className="relative cursor-pointer z-0"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+          >
+            <div className={`z-10 bg-photopicker-overlay-background h-60 w-60 absolute top-0 left-0 flex items-center rounded-full justify-center flex-col text-center gap-2
+            ${hover?"visible":"hidden"}
+            `}
+            id="context-opener"
+            onClick={(e)=> showContextMenu(e)}
+            >
+              <FaCamera className="text-2xl text-white" id="context-opener" 
+              onClick={(e)=>showContextMenu(e)}
+              />
+              <span onClick={(e)=>showContextMenu(e)}>Change <br/> Profile Photo</span>
+            </div>
+            <div className="flex items-center justify-center h-60 w-60">
+              <Image
+                src={image}
+                alt="avatar"
+                className="rounded-full" 
+                width={240}
+                height={240}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Avatar;
--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,20 +1,35 @@
+import Avatar from "@/components/common/Avatar";
+import Input from "@/components/common/Input";
 import { useStateProvider } from "@/context/StateContext";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function Onboarding() {
   const [{ userInfo }] = useStateProvider();
-  // console.log("userInfo:", userInfo);
+  const [name, setName] = useState(userInfo?.name || "");
+  const [about, setAbout] = useState("");
+  const [image, setImage] = useState("/default_avatar.png");
+
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center gap-2">
-        <Image src="/whatsapp.gif" alt="whatsapp" height={300} width={300} />
-        <span className="text-6xl">WhatsApp</span>
+      <div className="flex flex-col items-center justify-center gap-2 md:flex-row xs:mt-4 sm:mt-10 md:mt-6 lg:mt-12">
+        <Image
+          src="/whatsapp.gif"
+          alt="whatsapp"
+          height={300}
+          width={300}
+          className="w-32 h-32 md:w-60 md:h-60"
+        />
+        <span className="text-3xl md:text-6xl xs:mt-4 sm:mt-5 md:mt-0 lg:mt-0">WhatsApp</span>
       </div>
-      <div className="text-2xl">Create your profile</div>
-      <div className="flex gap-6 mt-6">
+      <div className="text-xl md:text-2xl mt-4">Create your profile</div>
+      <div className="flex flex-col md:flex-row gap-6 mt-6 items-center">
         <div className="flex flex-col items-center justify-center mt-5 gap-6">
-          {userInfo ? userInfo.name : "Loading..."}
+          <Input name="Display Name" state={name} setState={setName} label />
+          <Input name="About" state={about} setState={setAbout} label />
+        </div>
+        <div>
+          <Avatar type="xl" image={image} setImage={setImage} />
         </div>
       </div>
     </div>
